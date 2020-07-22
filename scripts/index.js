57f42882e7979ef12f2788e43031c4f37dce3e55
Anastasia Astashova <n-aneko23@yandex.ru>
2020-07-22 20:22:03 +0300

    Исправила замечания

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let element = document.querySelector('.edit-button');
+let popup = document.querySelector('.popup');
+let ProfileName = document.querySelector('.profile__name');
+let ProfileJob = document.querySelector('.profile__work');
+let formElement = popup.querySelector('.popup__container');
+let CloseButton = document.querySelector('.close-button');
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__job');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameInput.value = ProfileName.textContent;
+    jobInput.value = ProfileJob.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    ProfileName.textContent = nameInput.value;
+    ProfileJob.textContent = jobInput.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+element.addEventListener('click', openPopup);
+CloseButton.addEventListener('click', closePopup);
